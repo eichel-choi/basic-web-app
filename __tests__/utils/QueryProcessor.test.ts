09c8b93b5bb0edad3ec072378bb0b0ffbb098577
Eichel Choi <eichel.sc@gmail.com>
2023-09-19 16:13:33 -0400

    what is your nwhich of the following numbers is both a square and a cube: 1331, 4096, 144?

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -1,36 +1,55 @@
 import QueryProcessor from "../../utils/QueryProcessor";
-import '@testing-library/jest-dom'
 
 describe("QueryProcessor", () => {
-    test("should return a string", () => {
-        const query = "test";
-        const response: string = QueryProcessor(query);
-        expect(typeof response).toBe("string");
-    });
+  test("should return a string", () => {
+    const query = "test";
+    const response: string = QueryProcessor(query);
+    expect(typeof response).toBe("string");
+  });
 
-    test('should return shakespeare description', () => {
-        const query = "shakespeare";
-        const response: string = QueryProcessor(query);
-        expect(response).toBe((
-            "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-            "English poet, playwright, and actor, widely regarded as the greatest " +
-            "writer in the English language and the world's pre-eminent dramatist."
-          ));
-    });
+  test('should return Shakespeare description', () => {
+    const query = "shakespeare";
+    const response: string = QueryProcessor(query);
+    expect(response).toBe(
+      "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+      "English poet, playwright, and actor, widely regarded as the greatest " +
+      "writer in the English language and the world's pre-eminent dramatist."
+    );
+  });
 
-    test('should return Andrew ID', () => {
-        const query = "Andrew ID";
-        const response: string = QueryProcessor(query);
-        expect(response).toBe((
-            "sebinc"
-          ));
-    });
+  test('should return Andrew ID', () => {
+    const query = "andrew id";
+    const response: string = QueryProcessor(query);
+    expect(response).toBe("sebinc");
+  });
 
-    test('should return my name', () => {
-        const query = "name";
-        const response: string = QueryProcessor(query);
-        expect(response).toBe((
-            "Sebin (Eichel) Choi"
-          ));
-    });
+  test('should return name', () => {
+    const query = "name";
+    const response: string = QueryProcessor(query);
+    expect(response).toBe("Sebin (Eichel) Choi");
+  });
+
+  test('should return multiplication result', () => {
+    const query = "What is 5 multiplied by 3?";
+    const response: string = QueryProcessor(query);
+    expect(response).toBe("The result of 5 multiplied by 3 is 15.");
+  });
+
+  test('should return addition result', () => {
+    const query = "What is 7 plus 4?";
+    const response: string = QueryProcessor(query);
+    expect(response).toBe("The result of 7 plus 4 is 11.");
+  });
+
+  test('should return the largest number', () => {
+    const query = "Which of the following numbers is the largest: 21, 19, 32?";
+    const response: string = QueryProcessor(query);
+    expect(response).toBe("The largest number among 21, 19, and 32 is 32.");
+  });
+
+  test('should return square and cube numbers', () => {
+    const query = "Which of the following numbers is both a square and a cube: 1331, 4096, 144?";
+    const response: string = QueryProcessor(query);
+    expect(response).toBe("The numbers that are both a square and a cube are: 1331, 4096.");
+  });
 });
--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,16 +8,60 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("andrew id")) {
-    return (
-      "sebinc"
-    );
+    return "sebinc";
   }
 
   if (query.toLowerCase().includes("name")) {
-    return (
-      "Sebin (Eichel) Choi"
-    );
+    return "Sebin (Eichel) Choi";
   }
 
-  return "";
+  // Check if the query asks for multiplication or addition
+  if (query.toLowerCase().includes("multiplied by")) {
+    const match = query.match(/(\d+) multiplied by (\d+)/);
+    if (match && match.length === 3) {
+      const x = parseInt(match[1]);
+      const y = parseInt(match[2]);
+      const result = x * y;
+      return `The result of ${x} multiplied by ${y} is ${result}.`;
+    }
+  }
+
+  if (query.toLowerCase().includes("plus")) {
+    const match = query.match(/(\d+) plus (\d+)/);
+    if (match && match.length === 3) {
+      const x = parseInt(match[1]);
+      const y = parseInt(match[2]);
+      const result = x + y;
+      return `The result of ${x} plus ${y} is ${result}.`;
+    }
+  }
+
+  // Check if the query asks for finding the largest number
+  if (query.toLowerCase().includes("largest")) {
+    const numbersMatch = query.match(/(\d+), (\d+), (\d+)/);
+    if (numbersMatch && numbersMatch.length === 4) {
+      const x = parseInt(numbersMatch[1]);
+      const y = parseInt(numbersMatch[2]);
+      const z = parseInt(numbersMatch[3]);
+      const largest = Math.max(x, y, z);
+      return `The largest number among ${x}, ${y}, and ${z} is ${largest}.`;
+    }
+  }
+
+  // Check if the query asks for a square and cube number
+  if (query.toLowerCase().includes("square") && query.toLowerCase().includes("cube")) {
+    const numbersMatch = query.match(/(\d+), (\d+), (\d+), (\d+), (\d+), (\d+), (\d+)/);
+    if (numbersMatch && numbersMatch.length === 8) {
+      const numbers = numbersMatch.slice(1).map(Number);
+      const squareAndCubeNumbers = numbers.filter(num => Math.cbrt(num) === Math.sqrt(num));
+      if (squareAndCubeNumbers.length > 0) {
+        return `The numbers that are both a square and a cube are: ${squareAndCubeNumbers.join(", ")}.`;
+      } else {
+        return "None of the provided numbers are both a square and a cube.";
+      }
+    }
+  }
+
+  // If no matching condition is found, return an empty string or a default response
+  return "I'm sorry, I don't understand your query.";
 }
